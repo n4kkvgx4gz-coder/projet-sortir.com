fbe938e7ffb77a298b8a6906fbcf91065a1ccc6b
Youri Nerriec <n4kkvgx4gz@privaterelay.appleid.com>
2026-05-19 10:38:25 +0200

    entity + table sortie creation SortieController + twig sortie create ...

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $id_sortie = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $no = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateDebut = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateCloture = null;
+
+    #[ORM\Column]
+    private ?int $nbInscriptionsMax = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?bool $etat = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $urlPhoto = null;
+
+    #[ORM\Column]
+    private ?int $id_organisateur = null;
+
+    #[ORM\Column]
+    private ?int $id_lieu = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdSortie(): ?int
+    {
+        return $this->id_sortie;
+    }
+
+    public function setIdSortie(int $id_sortie): static
+    {
+        $this->id_sortie = $id_sortie;
+
+        return $this;
+    }
+
+    public function getNo(): ?string
+    {
+        return $this->no;
+    }
+
+    public function setNo(string $no): static
+    {
+        $this->no = $no;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTime
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTime $dateDebut): static
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateCloture(): ?\DateTime
+    {
+        return $this->dateCloture;
+    }
+
+    public function setDateCloture(\DateTime $dateCloture): static
+    {
+        $this->dateCloture = $dateCloture;
+
+        return $this;
+    }
+
+    public function getNbInscriptionsMax(): ?int
+    {
+        return $this->nbInscriptionsMax;
+    }
+
+    public function setNbInscriptionsMax(int $nbInscriptionsMax): static
+    {
+        $this->nbInscriptionsMax = $nbInscriptionsMax;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isEtat(): ?bool
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(bool $etat): static
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getUrlPhoto(): ?string
+    {
+        return $this->urlPhoto;
+    }
+
+    public function setUrlPhoto(?string $urlPhoto): static
+    {
+        $this->urlPhoto = $urlPhoto;
+
+        return $this;
+    }
+
+    public function getIdOrganisateur(): ?int
+    {
+        return $this->id_organisateur;
+    }
+
+    public function setIdOrganisateur(int $id_organisateur): static
+    {
+        $this->id_organisateur = $id_organisateur;
+
+        return $this;
+    }
+
+    public function getIdLieu(): ?int
+    {
+        return $this->id_lieu;
+    }
+
+    public function setIdLieu(int $id_lieu): static
+    {
+        $this->id_lieu = $id_lieu;
+
+        return $this;
+    }
+}
