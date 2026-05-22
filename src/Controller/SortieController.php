b136156c5fbea17d256ba0eb1ceb3c12b36c9d67
chrysantheme15 <chrysantheme.razafindrabodo2026@campus-eni.fr>
2026-05-22 14:28:21 +0200

    MAJ voir profil pour mettre les photos des participants

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-
 final class SortieController extends AbstractController
 {
     #[Route('/sortie', name: 'app_sortie')]
