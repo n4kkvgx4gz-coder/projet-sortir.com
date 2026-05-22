0c7874e2a4375b6620dd4419bad9d96a0eecd89d
Youri Nerriec <n4kkvgx4gz@privaterelay.appleid.com>
2026-05-22 13:45:16 +0200

    ajout en formulaire des catégories + campus ) + insert #[IsGranted('ROLE_USER')] sur route create pour redirection vers page de connexion si non connecté + gestion des aouts de villes + lieux si non dispo

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\Inscription;
 
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
